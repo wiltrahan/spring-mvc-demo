103c644536766a7adb52c33cda9178c08a13e6ed
wiltrahan <twilorip@gmail.com>
2018-04-16 19:13:18 -0400

    create controllers

---
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.luv2code.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller()
+public class HelloWorldController {
+	
+	
+	//need a controller method to show form
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	//need controller method to process form
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+}
